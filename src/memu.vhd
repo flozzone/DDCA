6147757571e73781f4e50d2841691c632dc3fda6
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2015-12-02 12:54:33 +0100

    Add memu testbench skeleton.
    memu is not clocked, therefore it will get quite simple to implement it
    and write a testbench for it.
    
    Signed-off-by: Florin Hillebrand <e0925917@student.tuwien.ac.at>

--- src/memu.vhd
+++ src/memu.vhd
@@ -7,13 +7,21 @@ use work.op_pack.all;
 
 entity memu is
 	port (
+		-- Access type
 		op   : in  mem_op_type;
+		-- Address
 		A    : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
+		-- Write data
 		W    : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		-- Data from memory
 		D    : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		-- Interface to memory
 		M    : out mem_out_type;
+		-- Result of memory load
 		R    : out std_logic_vector(DATA_WIDTH-1 downto 0);
+		-- Load exception
 		XL   : out std_logic;
+		-- Store exception
 		XS   : out std_logic);
 end memu;
 
