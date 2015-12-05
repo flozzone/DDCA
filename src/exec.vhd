c1ac544b043e70bb51f7b32e9a77f06ee60254c0
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2015-12-05 17:36:54 +0100

    Implement draft exec stage.
    Signed-off-by: Florin Hillebrand <e0925917@student.tuwien.ac.at>

--- src/exec.vhd
+++ src/exec.vhd
@@ -5,6 +5,8 @@ use ieee.numeric_std.all;
 use work.core_pack.all;
 use work.op_pack.all;
 
+use work.alu;
+
 entity exec is
 	
 	port (
@@ -33,9 +35,61 @@ entity exec is
 		exc_ovf          : out std_logic);
 
 end exec;
-
 architecture rtl of exec is
 
-begin  -- rtl
+signal alu_op : alu_op_type;
+signal alu_A : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal alu_B : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal alu_R : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal alu_Z : std_logic;
+signal alu_V : std_logic;
 
+begin  -- rtl
+	alu_inst : entity alu
+	port map (
+		op => alu_op,
+		A => alu_A,
+		B => alu_B,
+		R => alu_R,
+		Z => alu_Z,
+		V => alu_V
+	);
+
+	multiplex : process(clk, reset, stall, flush, op, pc_in, memop_in, jmpop_in, wbop_in, forwardA, forwardB, cop0_rddata, mem_aluresult, wb_result)
+	begin
+		if reset = '0' then
+			null;
+		elsif rising_edge(clk) and stall = '0' then
+			exc_ovf <= op.ovf;
+			rs <= op.rs;
+			rt <= op.rt;
+
+			pc_out <= pc_in;
+			memop_out <= memop_in;
+			jmpop_out <= jmpop_in;
+			wbop_out <= wbop_in;
+
+			-- TODO: ignore these signals for this assignment
+			-- forwardA
+			-- forwardB
+			-- mem_aluresult
+			-- wb_result
+
+			-- depends on instruction format
+			if op.regdst = '1' then
+				-- R format
+				rd <= op.rd;
+			else
+				-- others
+				rd <= op.rt;
+			end if;
+
+			-- aluresult
+			-- * aluresult <= mem_aluresult
+			-- * aluresult <= cop0_rddata for mfc0 instr
+			-- * aluresult <= pc_in (adjusted!? with ALU) for jal, jalr instr
+			-- * aluresult <= pc
+			
+		end if;
+	end process multiplex;
 end rtl;
