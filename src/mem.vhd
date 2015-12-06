7658d8821b35b0e8fa6b0cec29b426a4d8fef40c
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2015-12-06 13:46:42 +0100

    mem: implement with simple stall and without flush.
    Signed-off-by: Florin Hillebrand <e0925917@student.tuwien.ac.at>

--- src/mem.vhd
+++ src/mem.vhd
@@ -5,6 +5,9 @@ use ieee.numeric_std.all;
 use work.core_pack.all;
 use work.op_pack.all;
 
+use work.jmpu;
+use work.memu;
+
 entity mem is
 	
 	port (
@@ -36,6 +39,70 @@ end mem;
 
 architecture rtl of mem is
 
+signal jmp_op_int : jmp_op_type;
+signal jmp_N, jmp_Z, jmp_J : std_logic;
+
+signal memu_op : mem_op_type;
+signal memu_A : std_logic_vector(ADDR_WIDTH-1 downto 0);
+signal memu_W, memu_D, memu_R : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal memu_M : mem_out_type;
+signal memu_XL, memu_XS : std_logic;
+
 begin  -- rtl
+
+	jmpu_inst : entity jmpu
+	port map (
+		-- in
+		op => jmp_op_int,
+		N => jmp_N,
+		Z => jmp_Z,
+		--out
+		J => jmp_J
+	);
+
+	memu_inst : entity memu
+	port map (
+		-- in
+		op => memu_op,
+		A => memu_A,
+		W => memu_W,
+		D => memu_D,
+		-- out
+		M => memu_M,
+		R => memu_R,
+		XL => memu_XL,
+		XS => memu_XS
+	);
+
+	mem_proc : process(clk, reset, stall, flush, jmp_op, jmp_J, mem_op, pc_in, rd_in,
+		aluresult_in, wrdata, zero, neg, new_pc_in, wbop_in, mem_data,
+		memu_M, memu_R, memu_XL, memu_XS)
+	begin
+		if reset = '0' then
+			null;
+		elsif rising_edge(clk) and stall = '0' then
+			-- pass unchanged signals
+			pc_out <= pc_in;
+			rd_out <= rd_in;
+			aluresult_out <= aluresult_in;
+			new_pc_out <= new_pc_in;
+			wbop_out <= wbop_in;
+
+			-- jump
+			jmp_op_int <= jmp_op;
+			jmp_N <= neg;
+			jmp_Z <= zero;
+			pcsrc <= jmp_J;
+
+			-- memu
+			memu_op <= mem_op;
+			mem_out <= memu_M;
+			exc_load <= memu_XL;
+			exc_store <= memu_XS;
+			memu_W <= wrdata;
+			memu_D <= mem_data;
+			memresult <= memu_R;
+		end if;
+	end process mem_proc;
 
 end rtl;
