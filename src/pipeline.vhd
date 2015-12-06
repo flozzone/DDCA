029c8a873bc7090f064b53bb3075c962b18f99bc
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2015-12-06 16:11:07 +0100

    pipeline draft - not compiling

--- src/pipeline.vhd
+++ src/pipeline.vhd
@@ -17,5 +17,121 @@ end pipeline;
 architecture rtl of pipeline is
 	
 begin  -- rtl
-	
+
+	pipeline_proc : process
+	begin
+		clk, reset : in	 std_logic;
+		stall      : in  std_logic;
+		pcsrc	   : in	 std_logic;
+		pc_in	   : in	 std_logic_vector(PC_WIDTH-1 downto 0);
+		pc_out	   : out std_logic_vector(PC_WIDTH-1 downto 0);
+		instr	   : out std_logic_vector(INSTR_WIDTH-1 downto 0));
+
+		fetch_inst : entity fetch
+			port map(
+				-- in
+				clk =>
+				reset =>
+				pcsrc =>
+				pc_in =>
+				-- out
+				pc_out
+				instr
+			);
+
+			port (
+		clk, reset : in  std_logic;
+		stall      : in  std_logic;
+		flush      : in  std_logic;
+		pc_in      : in  std_logic_vector(PC_WIDTH-1 downto 0);
+		instr	   : in  std_logic_vector(INSTR_WIDTH-1 downto 0);
+		wraddr     : in  std_logic_vector(REG_BITS-1 downto 0);
+		wrdata     : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		regwrite   : in  std_logic;
+		pc_out     : out std_logic_vector(PC_WIDTH-1 downto 0);
+		exec_op    : out exec_op_type;
+		cop0_op    : out cop0_op_type;
+		jmp_op     : out jmp_op_type;
+		mem_op     : out mem_op_type;
+		wb_op      : out wb_op_type;
+		exc_dec    : out std_logic);
+
+		decode_inst : entity decode
+			port map(
+			);
+
+			port (
+		clk, reset       : in  std_logic;
+		stall      		 : in  std_logic;
+		flush            : in  std_logic;
+		pc_in            : in  std_logic_vector(PC_WIDTH-1 downto 0);
+		op	   	         : in  exec_op_type;
+		pc_out           : out std_logic_vector(PC_WIDTH-1 downto 0);
+		rd, rs, rt       : out std_logic_vector(REG_BITS-1 downto 0);
+		aluresult	     : out std_logic_vector(DATA_WIDTH-1 downto 0);
+		wrdata           : out std_logic_vector(DATA_WIDTH-1 downto 0);
+		zero, neg         : out std_logic;
+		new_pc           : out std_logic_vector(PC_WIDTH-1 downto 0);		
+		memop_in         : in  mem_op_type;
+		memop_out        : out mem_op_type;
+		jmpop_in         : in  jmp_op_type;
+		jmpop_out        : out jmp_op_type;
+		wbop_in          : in  wb_op_type;
+		wbop_out         : out wb_op_type;
+		forwardA         : in  fwd_type;
+		forwardB         : in  fwd_type;
+		cop0_rddata      : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		mem_aluresult    : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		wb_result        : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		exc_ovf          : out std_logic);
+
+		exec_inst : entity exec
+			port map(
+			);
+
+			port (
+		clk, reset    : in  std_logic;
+		stall         : in  std_logic;
+		flush         : in  std_logic;
+		mem_op        : in  mem_op_type;
+		jmp_op        : in  jmp_op_type;
+		pc_in         : in  std_logic_vector(PC_WIDTH-1 downto 0);
+		rd_in         : in  std_logic_vector(REG_BITS-1 downto 0);
+		aluresult_in  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		wrdata        : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		zero, neg     : in  std_logic;
+		new_pc_in     : in  std_logic_vector(PC_WIDTH-1 downto 0);
+		pc_out        : out std_logic_vector(PC_WIDTH-1 downto 0);
+		pcsrc         : out std_logic;
+		rd_out        : out std_logic_vector(REG_BITS-1 downto 0);
+		aluresult_out : out std_logic_vector(DATA_WIDTH-1 downto 0);
+		memresult     : out std_logic_vector(DATA_WIDTH-1 downto 0);
+		new_pc_out    : out std_logic_vector(PC_WIDTH-1 downto 0);
+		wbop_in       : in  wb_op_type;
+		wbop_out      : out wb_op_type;
+		mem_out       : out mem_out_type;
+		mem_data      : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		exc_load      : out std_logic;
+		exc_store     : out std_logic);
+
+		mem_inst : entity mem
+			port map(
+			);
+
+			port (
+		clk, reset : in  std_logic;
+		stall      : in  std_logic;
+		flush      : in  std_logic;
+		op	   	   : in  wb_op_type;
+		rd_in      : in  std_logic_vector(REG_BITS-1 downto 0);
+		aluresult  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		memresult  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		rd_out     : out std_logic_vector(REG_BITS-1 downto 0);
+		result     : out std_logic_vector(DATA_WIDTH-1 downto 0);
+		regwrite   : out std_logic);
+
+		wb_inst : entity wb
+			port map(
+			);
+	end process pipeline_proc;
 end rtl;
