e698bbb038bd103b005396630cd72407c3e939dd
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2015-12-08 20:18:25 +0100

    mem: use 2 process model.
    Signed-off-by: Florin Hillebrand <e0925917@student.tuwien.ac.at>

--- src/mem.vhd
+++ src/mem.vhd
@@ -39,70 +39,116 @@ end mem;
 
 architecture rtl of mem is
 
-signal jmp_op_int : jmp_op_type;
-signal jmp_N, jmp_Z, jmp_J : std_logic;
+signal int_mem_op        : mem_op_type;
+signal int_jmp_op        : jmp_op_type;
+signal int_pc_in         : std_logic_vector(PC_WIDTH-1 downto 0);
+signal int_rd_in         : std_logic_vector(REG_BITS-1 downto 0);
+signal int_aluresult_in  : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal int_wrdata        : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal int_zero, int_neg : std_logic;
+signal int_new_pc_in     : std_logic_vector(PC_WIDTH-1 downto 0);
+signal int_wbop_in       : wb_op_type;
+signal int_mem_data      : std_logic_vector(DATA_WIDTH-1 downto 0);
 
-signal memu_op : mem_op_type;
-signal memu_A : std_logic_vector(ADDR_WIDTH-1 downto 0);
-signal memu_W, memu_D, memu_R : std_logic_vector(DATA_WIDTH-1 downto 0);
-signal memu_M : mem_out_type;
-signal memu_XL, memu_XS : std_logic;
+signal int_jmp_J, int_jmp_zero, int_jmp_neg : std_logic;
+
+signal int_memu_A : std_logic_vector(ADDR_WIDTH-1 downto 0);
+signal int_memu_W, int_memu_D, int_memu_R : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal int_memu_M : mem_out_type;
+signal int_memu_XL, int_memu_XS : std_logic;
 
 begin  -- rtl
 
 	jmpu_inst : entity jmpu
 	port map (
 		-- in
-		op => jmp_op_int,
-		N => jmp_N,
-		Z => jmp_Z,
+		op => int_jmp_op,
+		N => int_jmp_zero,
+		Z => int_jmp_neg,
 		--out
-		J => jmp_J
+		J => int_jmp_J
 	);
 
 	memu_inst : entity memu
 	port map (
 		-- in
-		op => memu_op,
-		A => memu_A,
-		W => memu_W,
-		D => memu_D,
+		op => int_mem_op,
+		A => int_memu_A,
+		W => int_memu_W,
+		D => int_memu_D,
 		-- out
-		M => memu_M,
-		R => memu_R,
-		XL => memu_XL,
-		XS => memu_XS
+		M => int_memu_M,
+		R => int_memu_R,
+		XL => int_memu_XL,
+		XS => int_memu_XS
 	);
 
-	mem_proc : process(clk, reset, stall, flush, jmp_op, jmp_J, mem_op, pc_in, rd_in,
-		aluresult_in, wrdata, zero, neg, new_pc_in, wbop_in, mem_data,
-		memu_M, memu_R, memu_XL, memu_XS)
+	input : process(clk, reset)
 	begin
 		if reset = '0' then
-			null;
-		elsif rising_edge(clk) and stall = '0' then
-			-- pass unchanged signals
-			pc_out <= pc_in;
-			rd_out <= rd_in;
-			aluresult_out <= aluresult_in;
-			new_pc_out <= new_pc_in;
-			wbop_out <= wbop_in;
+			int_mem_op <= MEM_NOP;
+			int_jmp_op <= JMP_NOP;
+			int_pc_in <= (others => '0');
+			int_rd_in <= (others => '0');
+			int_aluresult_in <= (others => '0');
+			int_wrdata  <= (others => '0');
+			int_zero <= '0';
+			int_neg <= '0';
+			int_new_pc_in <= (others => '0');
+			int_wbop_in <= WB_NOP;
+			int_mem_data <= (others => '0');
+		elsif rising_edge(clk) then
+			if flush = '1' then
+				int_mem_op <= MEM_NOP;
+				int_jmp_op <= JMP_NOP;
+				int_pc_in <= (others => '0');
+				int_rd_in <= (others => '0');
+				int_aluresult_in <= (others => '0');
+				int_wrdata  <= (others => '0');
+				int_zero <= '0';
+				int_neg <= '0';
+				int_new_pc_in <= (others => '0');
+				int_wbop_in <= WB_NOP;
+				int_mem_data <= (others => '0');
+			elsif stall = '0' then
+				int_mem_op <= mem_op;
+				int_jmp_op <= jmp_op;
+				int_pc_in <= pc_in;
+				int_rd_in <= rd_in;
+				int_aluresult_in <= aluresult_in;
+				int_wrdata  <= wrdata;
+				int_zero <= zero;
+				int_neg <= neg; 
+				int_new_pc_in <= new_pc_in;
+				int_wbop_in <= wbop_in;
+				int_mem_data <= mem_data;
+			end if;
+		end if;
+	end process input;
+
+	mem_proc : process(int_pc_in, int_rd_in,
+		int_aluresult_in, int_wrdata, int_zero, int_neg, int_new_pc_in, int_wbop_in, int_mem_data,
+		int_memu_M, int_memu_R, int_memu_XL, int_memu_XS, int_jmp_J)
+	begin
+		-- pass unchanged signals
+		pc_out <= int_pc_in;
+		rd_out <= int_rd_in;
+		aluresult_out <= int_aluresult_in;
+		new_pc_out <= int_new_pc_in;
+		wbop_out <= int_wbop_in;
 
 			-- jump
-			jmp_op_int <= jmp_op;
-			jmp_N <= neg;
-			jmp_Z <= zero;
-			pcsrc <= jmp_J;
+		int_jmp_neg <= int_neg;
+		int_jmp_zero <= int_zero;
+		pcsrc <= int_jmp_J;
 
 			-- memu
-			memu_op <= mem_op;
-			mem_out <= memu_M;
-			exc_load <= memu_XL;
-			exc_store <= memu_XS;
-			memu_W <= wrdata;
-			memu_D <= mem_data;
-			memresult <= memu_R;
-		end if;
+		int_memu_A <= int_aluresult_in(ADDR_WIDTH-1 downto 0);
+		int_memu_W <= int_wrdata;
+		int_memu_D <= int_mem_data;
+		mem_out <= int_memu_M;
+		memresult <= int_memu_R;
+		exc_load <= int_memu_XL;
+		exc_store <= int_memu_XS;
 	end process mem_proc;
-
 end rtl;
