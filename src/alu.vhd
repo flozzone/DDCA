fa0f3ab983ae4a8770c8b0a441fefa9cb03a3966
Markus REISS <d1126188@ti10.tilab.tuwien.ac.at>
2015-12-11 17:03:01 +0100

    alu: worked on ALU_RSA TODO, changed formating of case statement

--- src/alu.vhd
+++ src/alu.vhd
@@ -27,92 +27,83 @@ begin --rtl
   cmd : process (op, A, B)
     begin
         R <= (others => '0');
-
+        V <= '0';
+        
         if (Signed(A) = 0) then
             Z <= '1';
         else
             Z <= '0';
         end if;
 
-        V <= '0';
-
-
         case op is
+            when ALU_NOP =>
+                R <= A;
 
+            when ALU_LUI =>
+                R <= std_logic_vector(shift_left(Signed(B),16));
 
+            when ALU_SLT =>
+                if Signed(A) < Signed(B) then
+                    R(0) <= '1';
+                else
+                    R(0) <= '0';
+                end if;
 
-        when ALU_NOP =>
-            R <= A;
+            when ALU_SLTU =>
+                if Unsigned(A) < Unsigned(B) then
+                    R(0) <= '1';
+                else
+                    R(0) <= '0';
+                end if;
 
-        when ALU_LUI =>
-            R <= std_logic_vector(shift_left(Signed(B),16));
+            when ALU_SLL =>
+                R <= std_logic_vector(shift_left(Unsigned(B),to_integer(signed(A(DATA_WIDTH_BITS-1 downto 0)))));
 
-        when ALU_SLT =>
-            if Signed(A) < Signed(B) then
-                R(0) <= '1';
-            else
-                R(0) <= '0';
-            end if;
+            when ALU_SRL =>
+                R <= std_logic_vector(shift_right(Unsigned(B),to_integer(signed(A(DATA_WIDTH_BITS-1 downto 0)))));
 
-        when ALU_SLTU =>
-            if Unsigned(A) < Unsigned(B) then
-                R(0) <= '1';
-            else
-                R(0) <= '0';
-            end if;
+            when ALU_SRA =>
+                R <= std_logic_vector(shift_right(Signed(B),to_integer(unsigned(A(DATA_WIDTH_BITS-1 downto 0)))));
+                
+            when ALU_ADD =>
+                R <= std_logic_vector(Signed(A)+Signed(B));
 
-        when ALU_SLL =>
-            R <= std_logic_vector(shift_left(Unsigned(B),to_integer(signed(A(DATA_WIDTH_BITS-1 downto 0)))));
+                if to_integer(Signed(A)) >= 0 and to_integer(Signed(B)) >= 0 and to_integer((Signed(A)+Signed(B))) < 0 then
+                    V <= '1';
+                elsif to_integer(Signed(A)) < 0 and to_integer(Signed(B)) < 0 and to_integer((Signed(A)+Signed(B))) >= 0 then
+                    V <= '1';
+                end if;
 
-        when ALU_SRL =>
-            R <= std_logic_vector(shift_right(Unsigned(B),to_integer(signed(A(DATA_WIDTH_BITS-1 downto 0)))));
+            when ALU_SUB =>
+                R <= std_logic_vector(Signed(A)-Signed(B));
 
-        when ALU_SRA =>
-        --TODO TO FIX?
-            R <= std_logic_vector(shift_right(Signed(B),to_integer(signed(A(DATA_WIDTH_BITS-1 downto 0)))));
-            --R(DATA_WIDTH_BITS-1) <= not std_logic(shift_right(Unsigned(B),to_integer(signed(A(DATA_WIDTH_BITS-1 downto 0))))(DATA_WIDTH_BITS-1));
+                if A = B then
+                    Z <= '1';
+                else
+                    Z <= '0';
+                end if;
 
-        when ALU_ADD =>
-            R <= std_logic_vector(Signed(A)+Signed(B));
+                if (Signed(A) >= 0 and Signed(B) < 0 and (Signed(A)-Signed(B)) < 0) then
+                    V <= '1';
+                elsif (Signed(A) < 0 and Signed(B) >= 0 and (Signed(A)-Signed(B)) >= 0) then
+                    V <= '1';
+                end if;
 
-            if to_integer(Signed(A)) >= 0 and to_integer(Signed(B)) >= 0 and to_integer((Signed(A)+Signed(B))) < 0 then
-                V <= '1';
-            elsif to_integer(Signed(A)) < 0 and to_integer(Signed(B)) < 0 and to_integer((Signed(A)+Signed(B))) >= 0 then
-                V <= '1';
-            end if;
+            when ALU_AND =>
+                R <= std_logic_vector(Signed(A) and Signed(B));
 
-        when ALU_SUB =>
-            R <= std_logic_vector(Signed(A)-Signed(B));
+            when ALU_OR =>
+                R <= std_logic_vector(Signed(A) or Signed(B));
 
-            if A = B then
-                Z <= '1';
-            else
-                Z <= '0';
-            end if;
+            when ALU_XOR =>
+                R <= std_logic_vector(Signed(A) xor Signed(B));
 
-            if (Signed(A) >= 0 and Signed(B) < 0 and (Signed(A)-Signed(B)) < 0) then
-                V <= '1';
-            elsif (Signed(A) < 0 and Signed(B) >= 0 and (Signed(A)-Signed(B)) >= 0) then
-                V <= '1';
-            end if;
+            when ALU_NOR =>
+                R <= not (std_logic_vector(Signed(A) or Signed(B)));
 
-        when ALU_AND =>
-            R <= std_logic_vector(Signed(A) and Signed(B));
-
-        when ALU_OR =>
-            R <= std_logic_vector(Signed(A) or Signed(B));
-
-        when ALU_XOR =>
-            R <= std_logic_vector(Signed(A) xor Signed(B));
-
-        when ALU_NOR =>
-            R <= not (std_logic_vector(Signed(A) or Signed(B)));
-
-        when others =>
-            null;
-
+            when others =>
+                null;
         end case;
-
     end process cmd;
 
 end rtl;
