7eeb59ebb096d48a01d53e18b7ab0efd729ca0b4
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2015-12-21 21:17:21 +0100

    level1: add other rddata values manually.
    All tests pass now, but well it should also work with internal ocram.
    
    Signed-off-by: Florin Hillebrand <e0925917@student.tuwien.ac.at>

--- nightly/level1/asm/clean.sh
+++ nightly/level1/asm/clean.sh
@@ -6,4 +6,8 @@
 # useful to jump to the line matching the test number
 #
 
-sed -i -e 's/#.*$//' -e '/^[[:blank:]]*$/d' -e '0,/^_start:$/d' $1
+tmp=$(mktemp)
+
+sed -e 's/#.*$//' -e '/^[[:blank:]]*$/d' -e '0,/^_start:$/d' $1 | sed -n '/.end _start/q;p' | sed 's/^[[:blank:]]*//g' > $tmp
+
+cat -n $tmp | sed 's/^[[:blank:]]*\([[:digit:]]+\)[[:blank:]]*\(.*\)/\1\#\2/g' > $2
