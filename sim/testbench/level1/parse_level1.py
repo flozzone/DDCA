40f47a520d06c435fe6d8cfc6367233974570a7a
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2015-12-21 21:32:24 +0100

    level1: adapt other testbenches according to new schema.
    Signed-off-by: Florin Hillebrand <e0925917@student.tuwien.ac.at>

--- sim/testbench/level1/parse_level1.py
+++ sim/testbench/level1/parse_level1.py
@@ -80,6 +80,7 @@ def main(result_file, test_file, rec_file=None):
         suite.addSignal("s_enable_ocram", 1, alias="ocram", default=True)
         suite.addSignal("s_reset", 1, alias="reset", default=True)
         suite.addSignal("s_mem_in.rddata", TestSuite.DATA_WIDTH, alias="rddata", default=0)
+        suite.addSignal("s_mem_in.busy", 1, alias="busy", default=False)
         suite.addSignal("a_mem_out.address", TestSuite.ADDR_WIDTH, alias="addr", default="---------------------")
         suite.addSignal("a_mem_out.rd", 1, alias="rd", default=False)
         suite.addSignal("a_mem_out.wr", 1, alias="wr", default=False)
